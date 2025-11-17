16bcfe500b8a39ec132a1cabda2a3093ca257921
sln <ratusha11th@gmail.com>
2025-11-17 18:12:26 +0500

    [1.0.10] CONDITION: developing  - Preparing for Math Update

---
+++ src/DEX/Swap/math_/OriginMath/UniswapV2Math.py
@@ -0,0 +1,5 @@
+from decimal import Decimal
+
+
+class UltimateUniswapV2Math:
+    pass
--- src/DEX/Swap/math_/OriginMath/__init__.py
+++ src/DEX/Swap/math_/OriginMath/__init__.py
@@ -1,5 +1,9 @@
+from src.DEX.Swap.math_.OriginMath.UniswapV2Math import UltimateUniswapV2Math
 from src.DEX.Swap.math_.OriginMath.UniswapV3Math import UltimateUniswapV3Math
 from src.DEX.Swap.math_.OriginMath.TraderJoeMath import UltimateTraderJoeMath
 
-__all__ = ['UltimateUniswapV3Math',
-           'UltimateTraderJoeMath']
+
+__all__ = [
+    'UltimateUniswapV2Math',
+    'UltimateUniswapV3Math',
+    'UltimateTraderJoeMath']
