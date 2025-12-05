9328e12855734daebada83aabdc5c1511d4f26b7
spyLn <ratusha11th@gmail.com>
2025-12-05 19:06:58 +0500

    [1.0.44] CONDITION: developing  - added OnlineSmartRouter Supervisor

---
+++ src/SoulSupervisors/EngineSupervisor.py
@@ -0,0 +1,53 @@
+import multiprocessing
+import os
+import time
+import logging
+from typing import Union, Type
+
+####################################
+from src.LoggerHandler.logger import setup_logger, get_logger
+from src.Config import config
+from src.SoulEngine.SmartRouter.OnlineSmartRouter import OnlineSmartRouter, OnlineSmartRouterConfigScheme
+####################################
+
+# OnlineSmartRouter Supervisor
+class OSRSupervisor:
+    osr_process_name = "OnlineSmartRouter"
+    CHECK_INTERVAL = 5
+
+    @staticmethod
+    def _start_online_smart_router():
+        conf = OnlineSmartRouterConfigScheme()
+        osr = OnlineSmartRouter(conf)
+        osr.start()
+
+    def _start_osr(self, logger: logging.Logger):
+        p = multiprocessing.Process(
+            target=self._start_online_smart_router,
+            name=OSRSupervisor.osr_process_name,
+        )
+        p.start()
+        logger.info(f"Started OnlineSmartRouter (PID: {p.pid})")
+        return p
+
+    def RUN_ONLINE_SMART_ROUTER(self):
+        logger_name = "OSRSupervisor"
+        log_file = os.path.join(config.LOG_MAIN_FOLDER, f"{logger_name}.log")
+        setup_logger(logger_name=logger_name, log_file=log_file)
+        logger = get_logger(logger_name=logger_name)
+
+        osr_worker: multiprocessing.Process = None
+
+        while True:
+            try:
+                if osr_worker is None:
+                    osr_worker = self._start_osr(logger)
+                elif not osr_worker.is_alive():
+                    logger.error("OnlineSmartRouter died. Restarting...")
+                    osr_worker.join()
+                    osr_worker = self._start_osr(logger)
+
+            except Exception as e:
+                logger.error(f"Supervisor error: {e}")
+
+            time.sleep(self.CHECK_INTERVAL)
---
+++ src/SoulSupervisors/__init__.py
@@ -0,0 +1,11 @@
+from src.SoulSupervisors.DataFetcherSupervisor import RUN_METADATA_FETCHERS, RUN_STATE_FETCHERS
+from src.SoulSupervisors.EngineSupervisor import OSRSupervisor
+
+__all__ = [
+    # DATA FETCHER SUPERVISORS
+    "RUN_METADATA_FETCHERS",
+    "RUN_STATE_FETCHERS",
+
+    # SOUL ENGINE SUPERVISORS
+    "OSRSupervisor",
+]
