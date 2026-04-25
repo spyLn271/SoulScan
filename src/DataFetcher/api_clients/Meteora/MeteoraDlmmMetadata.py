56a2a997bf3989255f68d2482d9b20f29c5d6d28
sln <ratusha11th@gmail.com>
2026-04-25 20:21:49 +0500

    Updated MeteoraDlmmMetadata.py to new API endpoint

--- src/DataFetcher/api_clients/Meteora/MeteoraDlmmMetadata.py
+++ src/DataFetcher/api_clients/Meteora/MeteoraDlmmMetadata.py
@@ -6,53 +6,12 @@ from src import Solana
 
 
 class MeteoraDlmmMetadata(PoolMetadataFetcher):
-    METEORA_URL = "https://dlmm-api.meteora.ag/pair/all?include_unknown=true"
+    METEORA_URL = "https://dlmm.datapi.meteora.ag/pools?page=1&page_size=500"
     MARKET, VERSION = config.MARKETS.get('meteora_dlmm').values()
 
     def __init__(self):
         conf = PoolMetadataConfigScheme(market=self.MARKET, version=self.VERSION)
         super().__init__(conf)
-
-    async def _get_decimals(self, target_mints: list) -> dict:
-        decimals_dict = {}
-        async with Solana(logger=self.logger) as solana:
-            mints = await solana.getMultipleMintAccounts(target_mints)
-
-        if not mints:
-            raise Exception(f"No mints found for {target_mints}")
-
-        for address, mint in mints.items():
-            try:
-                data = mint[0]['data']
-                decimals = data['decimals']
-                decimals_dict[address] = decimals
-            except Exception as e:
-                self.logger.error(e)
-                continue
-
-        if not decimals_dict:
-            raise Exception(f"No decimals found for {mints}")
-
-        return decimals_dict
-
-    def _derive_final_result_dict(self, pools: dict, decimals_dict: dict) -> dict:
-        final_result_dict = {}
-        for address, pool in pools.items():
-            try:
-                mint0 = pool["mint0"]
-                mint1 = pool["mint1"]
-
-                decimals0 = decimals_dict[mint0]
-                decimals1 = decimals_dict[mint1]
-                final_result_dict[address] = {**pool, **{'decimals0': decimals0, "decimals1": decimals1}}
-            except Exception as e:
-                self.logger.error(e)
-                continue
-
-        if not final_result_dict:
-            raise Exception(f"No valid pools found for {self.market} {self.version}.")
-
-        return final_result_dict
 
     async def _metadata_fetcher(self):
         pools = {}
@@ -63,25 +22,36 @@ class MeteoraDlmmMetadata(PoolMetadataFetcher):
                                 f"Response status: {response.status}")
 
             response_data = await response.json()
-            for pool in response_data:
+            for pool in response_data.get("data", []):
                 try:
                     is_blacklisted = pool["is_blacklisted"]
-                    trade_volume_24h = pool["trade_volume_24h"]
+                    trade_volume_24h = pool["volume"]["24"]
                     if is_blacklisted:
                         continue
                     if trade_volume_24h < config.MIN_VOL24:
                         continue
 
                     address = pool["address"]
-                    token0, token1 = pool["name"].split('-')
-                    mint0 = pool["mint_x"]
-                    mint1 = pool["mint_y"]
+
+                    token_x = pool["token_x"]
+                    token_y = pool["token_y"]
+
+                    token0 = token_x["symbol"]
+                    token1 = token_y["symbol"]
+
+                    mint0 = token_x["address"]
+                    mint1 = token_y["address"]
+
+                    decimals0 = token_x["decimals"]
+                    decimals1 = token_y["decimals"]
 
                     pools[address] = {
                         "token0": token0,
                         "token1": token1,
                         "mint0": mint0,
                         "mint1": mint1,
+                        "decimals0": decimals0,
+                        "decimals1": decimals1,
                         "is_blacklisted": is_blacklisted,
                         "volume24h": trade_volume_24h,
                         "dex": self.MARKET,
@@ -98,9 +68,4 @@ class MeteoraDlmmMetadata(PoolMetadataFetcher):
         if not target_mints:
             raise Exception(f"No valid pools found for {self.market} {self.version}.")
 
-        decimals_dict = await self._get_decimals(target_mints)
-        if not decimals_dict:
-            raise Exception(f"No decimals found for {self.market} {self.version}.")
-
-
-        return self._derive_final_result_dict(pools, decimals_dict)
+        return pool
